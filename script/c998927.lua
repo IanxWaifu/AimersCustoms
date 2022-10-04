137e307d9adc128a14a9125ea2da17eaca67ec8b
IanxWaifu <30645382+IanxWaifu@users.noreply.github.com>
2022-10-04 19:39:46 -0400

    Update

---
+++ script/c998927.lua
@@ -0,0 +1,251 @@
+--Scripted by IanxWaifu
+--Iron Saga - Polaris
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetOperation(s.regop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	-- Draw 1 Card
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_DRAW)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetCost(s.tgcost)
+	e4:SetTarget(s.tgtg)
+	e4:SetOperation(s.tgop)
+	c:RegisterEffect(e4)
+		aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_TO_HAND)
+		ge1:SetOperation(s.gchk)
+		Duel.RegisterEffect(ge1,0)
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_PHASE_START+PHASE_MAIN1)
+		ge2:SetCountLimit(1)
+		ge2:SetOperation(s.gclear)
+		Duel.RegisterEffect(ge2,0)
+	end)
+end
+s_g=Group.CreateGroup()
+s.listed_series={0x1A0}
+s.listed_names={id}
+function s.gchk(e,tp,eg,ev,ep,re,r,rp)
+	local c=eg:GetFirst()
+	while c do
+		if c:IsPreviousControler(tp) and c:IsMonster() and c:IsSetCard(0x1A0)then
+			s_g:AddCard(c) 
+		end
+		c=eg:GetNext()
+	end
+end
+function s.gclear(e,tp,eg,ev,ep,re,r,rp)
+	if s_g then
+		s_g:Clear()
+	end
+end
+function s.spfilter(c,ft)
+	return c:IsFaceup() and c:IsSetCard(0x1A0) and not c:IsCode(id) and c:IsAbleToHandAsCost()
+		and (ft>0 or c:GetSequence()<5)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,ft)
+	return ft>-1 and #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,nil,0)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local g=nil
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,ft)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_RTOHAND,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoHand(g,nil,REASON_COST)
+	g:DeleteGroup()
+end
+
+--ED Effect
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.acttg)
+	e1:SetOperation(s.actop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+
+function s.actfilter(c,tp)
+	return c:IsCode(998924) and c:GetActivateEffect():IsActivatable(tp,true,true) 
+end
+function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local n=#s_g
+	if chk==0 then return n>1 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(s.actfilter,tp,LOCATION_DECK,0,1,nil,tp) end 
+end
+function s.actop(e,tp,eg,ep,ev,re,r,rp)
+   if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+   		local sg=Duel.SelectMatchingCard(tp,s.actfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+		local n=#s_g
+		local tc=sg:GetFirst()
+		local te=tc:GetActivateEffect()
+		if not te or n<2 then return end
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
+		local pre={Duel.GetPlayerEffect(tp,EFFECT_CANNOT_ACTIVATE)}
+		if pre[1] then
+			for i,eff in ipairs(pre) do
+				local prev=eff:GetValue()
+				if type(prev)~='function' or prev(eff,te,tp) then return end
+			end
+		end
+		if tc then
+		Duel.HintSelection(sg)
+		te:UseCountLimit(tp,1)
+			local tpe=tc:GetType()
+			local tg=te:GetTarget()
+			local co=te:GetCost()
+			local op=te:GetOperation()
+			e:SetCategory(te:GetCategory())
+			e:SetProperty(te:GetProperty())
+			Duel.ClearTargetCard()
+			local loc=LOCATION_SZONE
+			if (tpe&TYPE_FIELD)~=0 then
+				loc=LOCATION_FZONE
+				local fc=Duel.GetFieldCard(1-tp,LOCATION_SZONE,5)
+				if Duel.IsDuelType(DUEL_1_FIELD) then
+					if fc then Duel.Destroy(fc,REASON_RULE) end
+					fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+					if fc and Duel.Destroy(fc,REASON_RULE)==0 then Duel.SendtoGrave(tc,REASON_RULE) end
+				else
+					fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+					if fc and Duel.SendtoGrave(fc,REASON_RULE)==0 then Duel.SendtoGrave(tc,REASON_RULE) end
+				end
+			end
+			if (tpe&TYPE_FIELD)==TYPE_FIELD then
+				Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+			else Duel.MoveToField(tc,tp,tp,loc,POS_FACEUP,true) end
+			Duel.Hint(HINT_CARD,0,tc:GetCode())
+			tc:CreateEffectRelation(te)
+			if (tpe&TYPE_EQUIP+TYPE_CONTINUOUS+TYPE_FIELD)==0 then
+				tc:CancelToGrave(false)
+			end
+			if te:GetCode()==EVENT_CHAINING then
+				local chain=Duel.GetCurrentChain()-1
+				local te2=Duel.GetChainInfo(chain,CHAININFO_TRIGGERING_EFFECT)
+				local tc=te2:GetHandler()
+				local g=Group.FromCards(tc)
+				local p=tc:GetControler()
+				if co then co(te,tp,g,p,chain,te2,REASON_EFFECT,p,1) end
+				if tg then tg(te,tp,g,p,chain,te2,REASON_EFFECT,p,1) end
+			elseif te:GetCode()==EVENT_FREE_CHAIN then
+				if co then co(te,tp,eg,ep,ev,re,r,rp,1) end
+				if tg then tg(te,tp,eg,ep,ev,re,r,rp,1) end
+			else
+				local res,teg,tep,tev,tre,tr,trp=Duel.CheckEvent(te:GetCode(),true)
+				if co then co(te,tp,teg,tep,tev,tre,tr,trp,1) end
+				if tg then tg(te,tp,teg,tep,tev,tre,tr,trp,1) end
+			end
+			Duel.BreakEffect()
+			local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+			if g then
+				local etc=g:GetFirst()
+				while etc do
+					etc:CreateEffectRelation(te)
+					etc=g:GetNext()
+				end
+			end
+			tc:SetStatus(STATUS_ACTIVATED,true)
+			if not tc:IsDisabled() then
+				if te:GetCode()==EVENT_CHAINING then
+					local chain=Duel.GetCurrentChain()-1
+					local te2=Duel.GetChainInfo(chain,CHAININFO_TRIGGERING_EFFECT)
+					local tc=te2:GetHandler()
+					local g=Group.FromCards(tc)
+					local p=tc:GetControler()
+					if op then op(te,tp,g,p,chain,te2,REASON_EFFECT,p) end
+				elseif te:GetCode()==EVENT_FREE_CHAIN then
+					if op then op(te,tp,eg,ep,ev,re,r,rp) end
+				else
+					local res,teg,tep,tev,tre,tr,trp=Duel.CheckEvent(te:GetCode(),true)
+					if op then op(te,tp,teg,tep,tev,tre,tr,trp) end
+				end
+			else
+				--insert negated animation here
+			end
+			Duel.RaiseEvent(Group.CreateGroup(tc),EVENT_CHAIN_SOLVED,te,0,tp,tp,Duel.GetCurrentChain())
+			if g and tc:IsType(TYPE_EQUIP) and not tc:GetEquipTarget() then
+				Duel.Equip(tp,tc,g:GetFirst())
+			end
+			tc:ReleaseEffectRelation(te)
+			if etc then
+				etc=g:GetFirst()
+				while etc do
+					etc:ReleaseEffectRelation(te)
+					etc=g:GetNext()
+				end
+			end
+		end
+	end
+end
+
+--BFG Draw
+function s.tgcostfilter(c)
+	return c:IsSetCard(0x1A0) and c:IsAbleToGraveAsCost() 
+end
+function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsPlayerCanDraw(tp,1) and aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,0)
+			and Duel.IsExistingMatchingCard(s.tgcostfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil)
+	end
+	aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local cg=Duel.SelectMatchingCard(tp,s.tgcostfilter,tp,LOCATION_HANDLOCATION_ONFIELD,0,1,1,nil)
+	Duel.SendtoGrave(cg,REASON_COST)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
