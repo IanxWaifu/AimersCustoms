0c953725053fc239b5ef168a5ba844347533f308
IanxWaifu <30645382+IanxWaifu@users.noreply.github.com>
2022-10-05 22:12:31 -0400

    Revelatias Fusions and more IS

--- script/c998933.lua
+++ script/c998933.lua
@@ -8,14 +8,30 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
 	--Send replace
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetCode(EFFECT_SEND_REPLACE)
-	e1:SetTarget(s.reptg)
-	e1:SetValue(s.repval)
-	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EFFECT_SEND_REPLACE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	e2:SetOperation(s.repop)
+	c:RegisterEffect(e2)
+	--Banish and Search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DISABLE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_HAND)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_SZONE+LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCost(aux.bfgcost)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
 end
 
 function s.repfilter(c,e,tp)
@@ -23,49 +39,57 @@ function s.repfilter(c,e,tp)
 	and  Duel.IsExistingMatchingCard(s.chkfilter,tp,LOCATION_MZONE,0,1,nil,e,tp)
 end
 function s.chkfilter(c,e,tp)
-	return c:IsFaceup() and c:IsSetCard(0x1A0) and c:IsCanBeEffectTarget(e)
+	return c:IsFaceup() and c:IsSetCard(0x1A0) and c:IsCanBeEffectTarget(e) and c:GetLevel()==7
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x1A0) and c:GetLevel()==4 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x1A0) and c:GetLevel()==7 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.reptestfilter(c,e,tp)
+	return c:IsSetCard(0x1A0) and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsCanBeEffectTarget(e) and  c:IsMonster() and c:GetLevel()==7
 end
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and (r&REASON_EFFECT+REASON_COST)~=0 and re and re:IsActiveType(TYPE_MONSTER)
 		and re:GetHandler():IsSetCard(0x1A0) and eg:IsExists(s.repfilter,1,nil,e,tp) end
-	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		local tc=Duel.SelectTarget(tp,s.chkfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
-		local c=e:GetHandler()
-		local g=eg:Filter(s.repfilter,nil,e,tp)
-		local ct=#g
-		if ct>1 then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-			g=g:Select(tp,1,ct,nil)
-		end
-		for tc in g:Iter() do
-			tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD&~RESET_TOHAND+RESET_PHASE+PHASE_END,0,1)
-		end
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-		e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		e1:SetCountLimit(1)
-		e1:SetCondition(s.spcon)
-		e1:SetOperation(s.spop)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e1,tp)
+	local g3=Duel.GetMatchingGroup(s.reptestfilter,tp,LOCATION_MZONE,0,nil,e,tp)
+	if #g3>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		local tg=Duel.SelectMatchingCard(tp,s.reptestfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+		local tg2=tg:GetFirst()
+		e:GetHandler():SetCardTarget(tg2)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		e:SetLabelObject(g:GetFirst())
 		return true
 	else return false end
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 end
 function s.repval(e,c)
-	return false
+	return true
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+end
+
+--Banish and Search
+function s.cfilter(c)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsType(TYPE_MONSTER) and c:IsSetCard(0x1A0)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil) 
 end
 function s.thfilter(c)
-	return c:GetFlagEffect(id)~=0
+	return c:IsLevel(4) and c:IsSetCard(0x1A0) and c:IsAbleToHand()
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.thfilter,1,nil)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(s.thfilter,nil)
-	Duel.ConfirmCards(1-tp,g)
-	Duel.ShuffleHand(tp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
---
+++ script/c998934.lua
@@ -0,0 +1,122 @@
+--Scripted by IanxWaifu
+--Iron Saga - Ruins of Reconception
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Negate Effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_SEND_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,2})
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	c:RegisterEffect(e2)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x1A0) and c:IsLevel(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		local tc=g:GetFirst()
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		if not tc:IsRelateToEffect(e) and not tc:IsFaceup() then return end
+			Duel.BreakEffect()
+			--effect gain
+			local e1=Effect.CreateEffect(tc)
+			e1:SetDescription(aux.Stringid(id,1))
+			e1:SetCategory(CATEGORY_TOHAND)
+			e1:SetType(EFFECT_TYPE_QUICK_O)
+			e1:SetCode(EVENT_FREE_CHAIN)
+			e1:SetCountLimit(1,{id,1})
+			e1:SetProperty(EFFECT_FLAG_DELAY)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetCost(s.announcecost)
+			e1:SetTarget(s.sptg2)
+			e1:SetOperation(s.spop2)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
+			tc:RegisterEffect(e1,true)
+	end
+end
+
+function s.announcecost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SendtoHand(e:GetHandler(),nil,REASON_COST)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function s.cfilter(c)
+	return c:IsSetCard(0x1A0) and c:IsAbleToRemoveAsCost()
+end
+
+function s.repfilter(c,e,tp)
+	return c:IsSetCard(0x1A0) and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:GetDestination()==LOCATION_HAND and c:IsMonster()
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x1A0) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+--Replace and Reduce
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) end
+	if chk==0 then return aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,0) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) 
+	and (r&REASON_EFFECT+REASON_COST)~=0 and re and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x1A0) and eg:IsExists(s.repfilter,1,nil,e,tp) end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+		g:AddCard(e:GetHandler())
+			local tc=eg:GetFirst()
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetValue(0)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_SET_DEFENSE)
+			tc:RegisterEffect(e2)
+			local g3=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
+			if Duel.Remove(g,POS_FACEUP,REASON_COST)~=0 and #g3>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then 
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local dg=g3:Select(tp,1,1,nil)
+			Duel.SpecialSummon(dg,0,tp,tp,false,false,POS_FACEUP)
+		end
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return true
+end
