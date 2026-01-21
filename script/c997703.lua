bf92948af65e7d5066fc22e79ec8d0fa04816ae0
IanxWaifu <30645382+IanxWaifu@users.noreply.github.com>
2026-01-21 18:48:53 -0500

    Stellarius Strapulate Fix
    Fixed an issue where attempting to Xyz Summon to a Zone when all Zones were occupied.

--- script/c997703.lua
+++ script/c997703.lua
@@ -74,54 +74,45 @@ function s.atcop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 
-function s.filter1(c,e,tp,lg,zones)
+function s.filter1(c,e,tp,lg,summonzones)
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
-	return (#pg<=0 or (#pg==1 and pg:IsContains(c))) and c:IsFaceup() and c:IsSetCard(0x12D9)
-		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,c:GetCode(),pg,zones) and lg:IsContains(c)
+	return (#pg<=0 or (#pg==1 and pg:IsContains(c))) and c:IsFaceup() and c:IsSetCard(0x12D9) and lg:IsContains(c)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,c:GetCode(),pg,summonzones)
 end
-function s.filter2(c,e,tp,mc,code,pg,zones)
+
+function s.filter2(c,e,tp,mc,code,pg,summonzones)
 	return mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and c:IsType(TYPE_XYZ) and c:IsSetCard(0x12D9) and not c:IsCode(code) and mc:IsCanBeXyzMaterial(c,tp)
-		and Duel.GetLocationCountFromEx(tp,tp,mc,c,zones)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+		and Duel.GetLocationCountFromEx(tp,tp,mc,c,summonzones)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
 end
+
 function s.spcon(e)
 	return e:GetHandler():GetSequence()>4
 end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	local lg=c:GetLinkedGroup()
 	lg:AddCard(c)
-	local zone={}
-	zone[0]=c:GetLinkedZone(0)
-	zone[1]=c:GetLinkedZone(1)
-	local zones=c:GetLinkedZone(tp)
-	local otherzones=~zones
-	-- If player already controls an EMZ monster, block both EMZs
-	if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsLocation,LOCATION_EMZONE),tp,LOCATION_EMZONE,0,1,nil) then
-	    otherzones = otherzones & (~0x60) 
-	end
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter1(chkc,e,tp,lg) end
-	if chk==0 then return Duel.GetLocationCountFromEx(tp,tp,nil,nil,~otherzones)>0 and Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp,lg,otherzones) end
+	local checkzones=c:GetLinkedZone(tp)
+	local summonzones=(~checkzones)&0x1f
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter1(chkc,e,tp,lg,summonzones) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp,lg,summonzones) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp,lg,otherzones)
+	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp,lg,summonzones)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,0,0)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
+	if not tc then return end
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(tc),tp,nil,nil,REASON_XYZ)
-	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) or #pg>1 or (#pg==1 and not pg:IsContains(tc)) then return end
-	local zone={}
-	zone[0]=c:GetLinkedZone(0)
-	zone[1]=c:GetLinkedZone(1)
-	local zones=c:GetLinkedZone(tp)
-	local otherzones=~zones
-	-- If player already controls an EMZ monster, block both EMZs
-	if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsLocation,LOCATION_EMZONE),tp,LOCATION_EMZONE,0,1,nil) then
-	    otherzones = otherzones & (~0x60) 
-	end
-	if Duel.GetLocationCountFromEx(tp,tp,tc,nil,~otherzones)<=0 or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
+	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) or #pg>1 or (#pg==1 and not pg:IsContains(tc)) then return end
+	local checkzones=c:GetLinkedZone(tp)
+	local summonzones=(~checkzones)&0x1f
+	if Duel.GetLocationCountFromEx(tp,tp,tc,nil,~summonzones)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetCode(),pg,otherzones)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetCode(),pg,summonzones)
 	local sc=g:GetFirst()
 	if sc then
 		local mg=tc:GetOverlayGroup()
@@ -130,8 +121,8 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		end
 		sc:SetMaterial(Group.FromCards(tc))
 		Duel.Overlay(sc,Group.FromCards(tc))
-		Duel.SpecialSummon(sc, SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP,otherzones)
+		Duel.SpecialSummon(
+		sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP,summonzones)
 		sc:CompleteProcedure()
 	end
 end
-
